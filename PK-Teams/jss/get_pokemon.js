ab39f892de6fc6beb8dc09e221d47630f4ca0b2b
kylehackett99 <50998448+kylehackett99@users.noreply.github.com>
2021-07-19 19:43:13 -0400

    Update get_pokemon.js

--- PK-Teams/jss/get_pokemon.js
+++ PK-Teams/jss/get_pokemon.js
@@ -6,7 +6,7 @@ const pokedex = document.getElementById("pokedex");
 const fetchPokemon = () => {
 
     var num = 807;
-    var max = 10157;
+    var max = 10158;
     var pokedex = new Array(num).fill({});
     const promises = [];
 
